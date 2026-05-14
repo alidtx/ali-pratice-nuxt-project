02d005999d1e16622e6e2238b9bff3075c75f346
alidtx <alirimon55@gmail.com>
2026-05-14 22:34:38 +0600

    modules pakages

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <script setup>
 import Card from '~/components/card.vue';
+import { storeToRefs } from 'pinia'
+import { useProductStore } from '#imports';
+
+const store = useProductStore();
+const { products } = storeToRefs(store)
 
 </script>
 <template>
      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-         <Card/>      
+         <Card v-for="product in products" :key="product.id"
+               
+      :product="product"
+      
+         />      
     </div>
 </template>
